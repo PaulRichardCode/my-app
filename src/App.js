5659dc574460906d9d9409c7f50328c00978d7fb
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-06-15 20:23:57 +0100

    sas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
+import sidebar from "./sidebar/sidebar";
+import home from "./home/home";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
