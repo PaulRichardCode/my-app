6210534c6923a2f1ef6377f9cb47de4b32b70602
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-06-22 12:20:06 +0100

    nb

--- src/sidebar/sidebar.jsx
+++ src/sidebar/sidebar.jsx
@@ -4,7 +4,11 @@ const sidebar = () => {
   return (
     <div>
       <h1>sidebar</h1>
-
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga harum aut
+        corporis? Quisquam fuga iusto, praesentium vel ipsum aliquid nisi id,
+        dolore hic qui rerum architecto dolores explicabo sit ad.
+      </p>
     </div>
   );
 };
