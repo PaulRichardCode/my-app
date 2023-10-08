73cc437039f6ba79212f905acd94dbe2d00e303d
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-10-08 18:55:46 +0100

    main sextion almost done

--- src/Home/main.jsx
+++ src/Home/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { FaArrowRight, FaSearchengin } from "react-icons/fa";
 import { GiGears } from "react-icons/gi";
-import { BsFillTriangleFill } from "react-icons/bs";
-import { MdSolarPower } from "react-icons/md";
+import { BsFillTriangleFill, BsCurrencyExchange } from "react-icons/bs";
+import { MdSolarPower, MdRealEstateAgent } from "react-icons/md";
 
 const Main = () => {
   return (
@@ -20,7 +20,7 @@ const Main = () => {
         create sustainable returns with lasting, positive impact for all our
         stakeholders.
       </p>
-      <div className="flex justify-evenly py-5 w-11/12 mx-auto">
+      <div className="flex justify-between py-5 w-11/12 mx-auto">
         <div className="flex-col w-1/6 ">
           <GiGears className="text-8xl text-blue-700 opacity-75 mx-auto" />
           <h2 className="text-2xl text-blue-950 py-2 w-24 leading-6 mx-auto my-2">
@@ -40,9 +40,18 @@ const Main = () => {
           </button>
         </div>
         <div className="flex-col w-1/6 ">
-          <MdSolarPower className="text-8xl text-blue-700 opacity-75 mx-auto" />
-          <h2 className="text-2xl text-center text-blue-950 py-2 leading-6 mx-auto my-2">
-            Private Infrastructure
+          <MdRealEstateAgent className="text-8xl text-blue-700 opacity-75 mx-auto" />
+          <h2 className="text-2xl text-center w-28 text-blue-950 py-2 leading-6 mx-auto my-2">
+            Private real estate
+          </h2>
+          <button className="font-semibold w-9/12 bg-blue-950 hover:bg-cyan-600 rounded-tl-lg rounded-br-lg transition duration-200 px-2 py-1 text-white flex justify-between items-center gap-1 mx-auto">
+            Find out more <BsFillTriangleFill className="rotate-90 text-xs " />
+          </button>
+        </div>
+        <div className="flex-col w-1/6 ">
+          <BsCurrencyExchange className="text-8xl text-blue-700 opacity-75 mx-auto" />
+          <h2 className="text-2xl text-center w-28 text-blue-950 py-2 leading-6 mx-auto my-2">
+            Private debt
           </h2>
           <button className="font-semibold w-9/12 bg-blue-950 hover:bg-cyan-600 rounded-tl-lg rounded-br-lg transition duration-200 px-2 py-1 text-white flex justify-between items-center gap-1 mx-auto">
             Find out more <BsFillTriangleFill className="rotate-90 text-xs " />
