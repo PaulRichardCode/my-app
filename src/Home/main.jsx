5a2dd06e78bb32c480723aef8f8479382e9e5ce0
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-10-08 17:47:38 +0100

    making progress

--- src/Home/main.jsx
+++ src/Home/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div>
-      <h1 className="text-blue-700 font-medium md:text-4xl text-left">
+    <div className="w-11/12 mx-auto">
+      <h1 className="text-blue-700 font-medium md:text-4xl py-5 text-left">
         What we do
       </h1>
-      <p className="font-medium text-xl">
+      <p className="font-medium text-xl text-left py-10">
         Partners Group is a leading global private markets firm. Since 1996, we
         have invested over USD 200 billion in private equity, private debt,
         private real estate, and private infrastructure on behalf of our clients
@@ -16,11 +16,15 @@ const Main = () => {
         create sustainable returns with lasting, positive impact for all our
         stakeholders.
       </p>
-      <div className="flex">
-        <div className="flex-col">
+      <div className="flex py-5 w-5/6 mx-auto">
+        <div className="flex-col w-1/6 bg-yellow-200">
           <img src="" alt="" />
-          <h2>Private equity</h2>
-          <button>Find Out More</button>
+          <h2 className="text-2xl text-blue-950 py-2">
+            Private <br /> equity
+          </h2>
+          <button className="font-bold bg-blue-950 px-2 py-1 text-white">
+            Find out more
+          </button>
         </div>
       </div>
     </div>
