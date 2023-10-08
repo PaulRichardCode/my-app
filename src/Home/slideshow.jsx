27919d16920ba7b49b680703bf0b06e5b8dbe305
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-10-08 17:28:44 +0100

    adding slideshow

--- src/Home/slideshow.jsx
+++ src/Home/slideshow.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+import house from "../res/houseLight.jpg";
+// Import the CSS for styling
+
+const divStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundSize: "cover",
+  height: "400px",
+};
+const slideImages = [
+  {
+    url: "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    caption: "Slide 1",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
+    caption: "Slide 2",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1594818379496-da1e345b0ded?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHNvbGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    caption: "Slide 3",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1587168173357-99c7e95fb5cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8d2luZCUyMHR1cmJpbmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    caption: "Slide 3",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1531971589569-0d9370cbe1e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG91c2UlMjBsaWdodHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    caption: "Slide 3",
+  },
+];
 
 const Slideshow = () => {
   return (
-    <div>
-      <img src="" alt="" />
+    <div className="slide-container py-5">
+      <Slide>
+        {slideImages.map((slideImage, index) => (
+          <div key={index}>
+            <div
+              style={{
+                ...divStyle,
+                backgroundImage: `url(${slideImage.url})`,
+              }}></div>
+          </div>
+        ))}
+      </Slide>
     </div>
   );
 };
